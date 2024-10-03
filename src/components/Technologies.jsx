814770c76db82e849fb0db76a461826de2f09705
kenneth <kennytakudzwa@gmail.com>
2024-10-03 18:13:31 +0200

    minor changes

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -28,7 +28,7 @@ const Technologies = () => {
         transition={{ duration: 1.5 }}
         className="my-20 text-center text-4xl"
       >
-        Technologies
+        What I am involved in...
       </motion.h1>
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
