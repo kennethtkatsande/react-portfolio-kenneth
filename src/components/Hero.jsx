0afa9c2215c0f4f6bfc3309bee9c198e2e19d8d9
kenneth <kennytakudzwa@gmail.com>
2024-10-03 17:58:34 +0200

    resource updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from '../constants'
-import profilePic from '../assets/kevinRushProfile.png'
+import profilePic from '../assets/my_formal_profile.jng'
 import { motion } from 'framer-motion'
 
 const container = (delay) => ({
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,76 +1,75 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import project1 from '../assets/projects/project-1.jpg'
+import project2 from '../assets/projects/car_on_sale.jpg'
+import project3 from '../assets/projects/maize_stack.jpg'
+import project4 from '../assets/projects/amazon_book.jpg'
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I’m a versatile professional with a passion for teaching, business, and creativity. As a tutor specializing in South African Physical Science, I enjoy guiding students toward academic success. I also work as an agent in both car sales and grain distribution, especially to Zimbabwe. In addition, I build personalized portfolio websites. My creative side shines through my poetry, with two books available on Amazon: Have you ever wondered what they do, my boy? and Chiselled Thoughts. Let’s connect and explore how I can help you achieve your goals!.`
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a multi-faceted professional passionate about continuous personal development and sharing knowledge. As a South African Physical Science tutor, I enjoy helping students excel, while my work as an agent in car sales and grain distribution, particularly to Zimbabwe, allows me to connect people with essential products. I also create custom portfolio websites to help individuals showcase their unique talents. My creative journey as a poet has led to two published works, *Have you ever wondered what they do, my boy?* and *Chiselled Thoughts*, available on Amazon. I believe life is a challenge best shared with those who can uplift one another, bringing fulfillment to all involved..`
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: '2020 - Present',
+    role: 'Tutor in Maths and Physical Science',
+    company: 'Metropolitan Group of Schools & Mount Sunset Private School',
+    description: `Teaching Pure Mathematics and Physical Science.`,
+    technologies: [
+      'Mathematics',
+      'Physical Science',
+      'Statistics',
+      'A-Level',
+      'Grade 12',
+    ],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: '2020 - present',
+    role: 'Poet',
+    company: 'Quill Stroking Frenzy (my brand name for poetry)',
+    description: `I write poems for encouragement, current events and for giving another point of view.`,
+    technologies: ['Motivation', 'Advice', 'Life Events', 'Publishing'],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    year: 'Dec 2023 - present',
+    role: 'Portfolio Website Developer',
+    company: 'Freelance',
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. `,
+    technologies: ['Python', 'Typescript', 'JavaScript', 'HTML', 'CSS'],
   },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
+]
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: 'Portfolio Website',
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      'A fully functional portfolio website featuring modern design paradigms and showcasing projects, skills, and contact information',
+    technologies: ['HTML', 'CSS', 'React', 'Node.js'],
   },
   {
-    title: "Task Management App",
+    title: 'Selling Quality Cars',
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      'If you are looking for your first ride or one that saves fuel and always impresses your friends let me be at your service. Be it a bakkie, a hatchback, I got you ...',
+    technologies: ['Petrol', 'Diesel', 'Hybrid'],
   },
   {
-    title: "Portfolio Website",
+    title: 'Maize',
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      'If you are ready to supply maize outside Mzansi, get in touch so that we get you the best deal per tonne for your noble work.',
+    technologies: ['Maize'],
   },
   {
-    title: "Blogging Platform",
+    title: 'Poetry',
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      'For assistance in assignments that involve poetry, written pieces for special events for loved ones, birthdays and so forth, get in touch.',
+    technologies: ['Birthday', 'Encouragement', 'Memory', 'Performance'],
   },
-];
+]
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
-};
+  address: 'Plot No 7 Katoen, White River ',
+  phoneNo: '+27 73 391 4508 ',
+  email: 'kennethtakudzwakatsande@gmail.com',
+}
